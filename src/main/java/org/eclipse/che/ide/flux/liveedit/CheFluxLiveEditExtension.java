d6abd51ea98dc31e12e5a5a14684f2a9974ca8d3
Sun Seng David TAN <sunix@sunix.org>
2015-06-30 09:05:37 +0200

    java8

--- src/main/java/org/eclipse/che/ide/flux/liveedit/CheFluxLiveEditExtension.java
+++ src/main/java/org/eclipse/che/ide/flux/liveedit/CheFluxLiveEditExtension.java
@@ -14,29 +14,19 @@ package org.eclipse.che.ide.flux.liveedit;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.annotation.Nonnull;
-
+import org.eclipse.che.api.runner.ApplicationStatus;
 import org.eclipse.che.api.runner.dto.ApplicationProcessDescriptor;
 import org.eclipse.che.api.runner.gwt.client.RunnerServiceClient;
 import org.eclipse.che.ide.api.event.OpenProjectEvent;
-import org.eclipse.che.ide.api.event.OpenProjectHandler;
 import org.eclipse.che.ide.api.extension.Extension;
 import org.eclipse.che.ide.collections.Array;
 import org.eclipse.che.ide.ext.runner.client.callbacks.AsyncCallbackBuilder;
-import org.eclipse.che.ide.ext.runner.client.callbacks.FailureCallback;
-import org.eclipse.che.ide.ext.runner.client.callbacks.SuccessCallback;
-import org.eclipse.che.ide.ext.runner.client.models.Runner;
 import org.eclipse.che.ide.ext.runner.client.runneractions.impl.GetRunningProcessesAction;
 import org.eclipse.che.ide.ext.runner.client.runneractions.impl.launch.common.RunnerApplicationStatusEvent;
-import org.eclipse.che.ide.ext.runner.client.runneractions.impl.launch.common.RunnerApplicationStatusEventHandler;
 import org.eclipse.che.ide.jseditor.client.document.Document;
-import org.eclipse.che.ide.jseditor.client.document.DocumentHandle;
 import org.eclipse.che.ide.jseditor.client.events.DocumentChangeEvent;
-import org.eclipse.che.ide.jseditor.client.events.DocumentChangeHandler;
 import org.eclipse.che.ide.jseditor.client.events.DocumentReadyEvent;
-import org.eclipse.che.ide.jseditor.client.events.DocumentReadyHandler;
 import org.eclipse.che.ide.rest.DtoUnmarshallerFactory;
-import org.eclipse.che.ide.socketio.Consumer;
 import org.eclipse.che.ide.socketio.SocketIOOverlay;
 import org.eclipse.che.ide.socketio.SocketIOResources;
 import org.eclipse.che.ide.socketio.SocketOverlay;
@@ -61,86 +51,92 @@ public class CheFluxLiveEditExtension {
 
     private boolean               isUpdatingModel = false;
 
+    private Provider<AsyncCallbackBuilder<Array<ApplicationProcessDescriptor>>> callbackBuilderProvider;
+
+    private DtoUnmarshallerFactory dtoUnmarshallerFactory;
+
+    private EventBus eventBus;
+
+    private RunnerServiceClient runnerService;
+
 
     @Inject
     public CheFluxLiveEditExtension(EventBus eventBus, final RunnerServiceClient runnerService, final Provider<AsyncCallbackBuilder<Array<ApplicationProcessDescriptor>>> callbackBuilderProvider, final DtoUnmarshallerFactory dtoUnmarshallerFactory) {
 
+        this.eventBus = eventBus;
+        this.runnerService = runnerService;
+        this.callbackBuilderProvider = callbackBuilderProvider;
+        this.dtoUnmarshallerFactory = dtoUnmarshallerFactory;
+
         SocketIOResources ioresources = GWT.create(SocketIOResources.class);
         ScriptInjector.fromString(ioresources.socketIo().getText()).setWindow(ScriptInjector.TOP_WINDOW).inject();
 
-        eventBus.addHandler(OpenProjectEvent.TYPE, new OpenProjectHandler() {
-            @Override
-            public void onOpenProject(OpenProjectEvent event) {
-                runnerService.getRunningProcesses(event.getProjectName(), callbackBuilderProvider
-                                                                                                 .get()
-                                                                                                 .unmarshaller(dtoUnmarshallerFactory.newArrayUnmarshaller(ApplicationProcessDescriptor.class))
-                                                                                                 .success(new SuccessCallback<Array<ApplicationProcessDescriptor>>() {
-                                                                                                     @Override
-                                                                                                     public void onSuccess(Array<ApplicationProcessDescriptor> result) {
-                                                                                                         if (result.isEmpty()) {
-                                                                                                             return;
-                                                                                                         }
-                                                                                                         for (ApplicationProcessDescriptor applicationProcessDescriptor : result.asIterable()) {
-                                                                                                             if (connectIfFluxMicroservice(applicationProcessDescriptor)) {
-                                                                                                                 break;
-                                                                                                             }
-                                                                                                         }
+        eventBus.addHandler(OpenProjectEvent.TYPE,
+                            openProjectEvent -> {
+                                runnerService.getRunningProcesses(openProjectEvent.getProjectName(),
+                                                                  newAsyncCallback()
+                                                                                    .success(
+                                                                                             processDescriptors -> {
+                                                                                                 if (processDescriptors.isEmpty()) {
+                                                                                                     return;
+                                                                                                 }
+                                                                                                 for (ApplicationProcessDescriptor applicationProcessDescriptor : processDescriptors.asIterable()) {
+                                                                                                     if (connectIfFluxMicroservice(applicationProcessDescriptor)) {
+                                                                                                         break;
                                                                                                      }
-                                                                                                 })
-                                                                                                 .failure(new FailureCallback() {
-                                                                                                     @Override
-                                                                                                     public void onFailure(@Nonnull Throwable reason) {
-                                                                                                         Log.error(GetRunningProcessesAction.class, reason);
-                                                                                                     }
-                                                                                                 })
-                                                                                                 .build());
-            }
-        });
+                                                                                                 }
+                                                                                             })
+                                                                                    .failure(e -> Log.error(GetRunningProcessesAction.class, e))
+                                                                                    .build());
 
-        eventBus.addHandler(RunnerApplicationStatusEvent.TYPE, new RunnerApplicationStatusEventHandler() {
-            @Override
-            public void onRunnerStatusChanged(@Nonnull Runner runner) {
-                ApplicationProcessDescriptor descriptor = runner.getDescriptor();
-                connectIfFluxMicroservice(descriptor);
-            }
+                            });
 
-        });
+
+
+        eventBus.addHandler(RunnerApplicationStatusEvent.TYPE,
+                            runner -> {
+                                ApplicationProcessDescriptor descriptor = runner.getDescriptor();
+                                connectIfFluxMicroservice(descriptor);
+                            });
 
 
         eventBus.addHandler(DocumentReadyEvent.TYPE,
-                            new DocumentReadyHandler() {
-                                @Override
-                                public void onDocumentReady(DocumentReadyEvent event) {
-                                    liveDocuments.put(event.getDocument().getFile().getPath(), event.getDocument());
-
-                                    final DocumentHandle documentHandle = event.getDocument().getDocumentHandle();
-
-                                    documentHandle.getDocEventBus().addHandler(DocumentChangeEvent.TYPE, new DocumentChangeHandler() {
-                                        @Override
-                                        public void onDocumentChange(DocumentChangeEvent event) {
-                                            if (socket != null) {
-                                                // full path start with /, so substring
-                                                String fullPath = event.getDocument().getDocument().getFile().getPath().substring(1);
-                                                String project = fullPath.substring(0, fullPath.indexOf('/'));
-                                                String resource = fullPath.substring(fullPath.indexOf('/') + 1);
-                                                String text = JsonUtils.escapeValue(event.getText());
-                                                String json = "{"
-                                                              + "\"username\":\"USER\","
-                                                              + "\"project\":\"" + project + "\","
-                                                              + "\"resource\":\"" + resource + "\","
-                                                              + "\"offset\":" + event.getOffset() + ","
-                                                              + "\"removedCharCount\":" + event.getRemoveCharCount() + ","
-                                                              + "\"addedCharacters\":" + text + "}";
-                                                if (isUpdatingModel) {
-                                                    return;
-                                                }
-                                                socket.emit("liveResourceChanged", JsonUtils.unsafeEval(json));
-                                            }
-                                        }
-                                    });
-                                }
+                            docReadyEvent -> {
+                                liveDocuments.put(docReadyEvent.getDocument().getFile().getPath(), docReadyEvent.getDocument());
+                                docReadyEvent
+                                             .getDocument()
+                                             .getDocumentHandle()
+                                             .getDocEventBus()
+                                             .addHandler(DocumentChangeEvent.TYPE,
+                                                         docChangeEvent -> {
+                                                             if (socket != null) {
+                                                                 // full path start with /, so substring
+                                                         String fullPath = docChangeEvent.getDocument().getDocument().getFile().getPath().substring(1);
+                                                         String project = fullPath.substring(0, fullPath.indexOf('/'));
+                                                         String resource = fullPath.substring(fullPath.indexOf('/') + 1);
+                                                         String text = JsonUtils.escapeValue(docChangeEvent.getText());
+                                                         String json = "{"
+                                                                       + "\"username\":\"USER\","
+                                                                       + "\"project\":\"" + project + "\","
+                                                                       + "\"resource\":\"" + resource + "\","
+                                                                       + "\"offset\":" + docChangeEvent.getOffset() + ","
+                                                                       + "\"removedCharCount\":" + docChangeEvent.getRemoveCharCount() + ","
+                                                                       + "\"addedCharacters\":" + text + "}";
+                                                         if (isUpdatingModel) {
+                                                             return;
+                                                         }
+                                                         socket.emit("liveResourceChanged", JsonUtils.unsafeEval(json));
+                                                     }
+                                                 }
+                                             );
                             });
 
+    }
+
+    private AsyncCallbackBuilder<Array<ApplicationProcessDescriptor>> newAsyncCallback(){
+        return callbackBuilderProvider
+            .get()
+            .unmarshaller(dtoUnmarshallerFactory.newArrayUnmarshaller(ApplicationProcessDescriptor.class));
     }
 
     private boolean connectIfFluxMicroservice(ApplicationProcessDescriptor descriptor) {
@@ -153,15 +149,9 @@ public class CheFluxLiveEditExtension {
         }
         String host = descriptor.getPortMapping().getHost();
 
-        switch (descriptor.getStatus()) {
-            case RUNNING:
-                connectToFlux(descriptor, host, fluxPort);
-                return true;
-            case FAILED:
-            case STOPPED:
-            case CANCELLED:
-                break;
-            default:
+        if (descriptor.getStatus() == ApplicationStatus.RUNNING) {
+            connectToFlux(descriptor, host, fluxPort);
+            return true;
         }
         return false;
     }
@@ -181,31 +171,25 @@ public class CheFluxLiveEditExtension {
                     @Override
                     public void run() {
                         socket.emit("connectToChannel", JsonUtils.safeEval("{\"channel\" : \"USER\"}"));
-
-                        socket.on("liveResourceChanged", new Consumer<FluxResourceChangedEventDataOverlay>() {
-                            @Override
-                            public void accept(FluxResourceChangedEventDataOverlay event) {
-                                Document document = liveDocuments.get("/" + event.getProject() + "/" + event.getResource());
-                                if (document == null) {
-                                    return;
-                                }
-                                isUpdatingModel = true;
-                                document.replace(event.getOffset(), event.getRemovedCharCount(), event.getAddedCharacters());
-                                isUpdatingModel = false;
-                            }
-                        });
-
+                        socket.on("liveResourceChanged",
+                                  (FluxResourceChangedEventDataOverlay event) -> {
+                                      Document document = liveDocuments.get("/" + event.getProject() + "/" + event.getResource());
+                                      if (document == null) {
+                                          return;
+                                      }
+                                      isUpdatingModel = true;
+                                      document.replace(event.getOffset(), event.getRemovedCharCount(), event.getAddedCharacters());
+                                      isUpdatingModel = false;
+                                  });
                     }
                 }.schedule(1000);
 
             }
         }.schedule(5000);
-
-
     }
 
     public static native SocketIOOverlay getSocketIO()/*-{
-                                                      return $wnd.io;
-                                                      }-*/;
+        return $wnd.io;
+    }-*/;
 
 }
