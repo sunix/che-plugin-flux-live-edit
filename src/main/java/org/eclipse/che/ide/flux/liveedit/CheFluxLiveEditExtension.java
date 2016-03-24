78659534435074d80eceb5dffb96269bdeef7ad5
Sun Seng David TAN <sun.tan@serli.com>
2016-03-24 08:04:04 +0000

    Refactoring: make the code a bit easier to read

--- src/main/java/org/eclipse/che/ide/flux/liveedit/CheFluxLiveEditExtension.java
+++ src/main/java/org/eclipse/che/ide/flux/liveedit/CheFluxLiveEditExtension.java
@@ -70,6 +70,14 @@ public class CheFluxLiveEditExtension {
 
     private CommandPropertyValueProviderRegistry commandPropertyValueProviderRegistry;
 
+    private EventBus                             eventBus;
+
+    private AppContext                           appContext;
+
+    private MachineServiceClient                 machineServiceClient;
+
+    private DtoUnmarshallerFactory               dtoUnmarshallerFactory;
+
     @Inject
     public CheFluxLiveEditExtension(final MessageBusProvider messageBusProvider,
                                     final EventBus eventBus,
@@ -79,23 +87,38 @@ public class CheFluxLiveEditExtension {
                                     final AppContext appContext,
                                     final CommandManager commandManager,
                                     final CommandPropertyValueProviderRegistry commandPropertyValueProviderRegistry) {
+        this.dtoUnmarshallerFactory = dtoUnmarshallerFactory;
         this.commandManager = commandManager;
         this.messageBus = messageBusProvider.getMessageBus();
         this.commandPropertyValueProviderRegistry = commandPropertyValueProviderRegistry;
+        this.eventBus = eventBus;
+        this.appContext = appContext;
+        this.machineServiceClient = machineServiceClient;
 
         injectSocketIO();
 
-        eventBus.addHandler(ProjectExplorerLoadedEvent.getType(), new ProjectExplorerLoadedEvent.ProjectExplorerLoadedHandler() {
+        connectToFluxOnProjectLoaded();
 
+        connectToFluxOnFluxProcessStarted();
+
+        sendFluxMessageOnDocumentModelChanged();
+    }
+
+
+    private void injectSocketIO() {
+        SocketIOResources ioresources = GWT.create(SocketIOResources.class);
+        ScriptInjector.fromString(ioresources.socketIo().getText()).setWindow(ScriptInjector.TOP_WINDOW).inject();
+    }
+
+    private void connectToFluxOnProjectLoaded() {
+        eventBus.addHandler(ProjectExplorerLoadedEvent.getType(), new ProjectExplorerLoadedEvent.ProjectExplorerLoadedHandler() {
             @Override
             public void onProjectsLoaded(ProjectExplorerLoadedEvent event) {
-
                 String machineId = appContext.getDevMachineId();
                 Promise<List<MachineProcessDto>> processesPromise = machineServiceClient.getProcesses(machineId);
                 processesPromise.then(new Operation<List<MachineProcessDto>>() {
                     @Override
                     public void apply(final List<MachineProcessDto> descriptors) throws OperationException {
-
                         if (descriptors.isEmpty()) {
                             return;
                         }
@@ -105,22 +128,96 @@ public class CheFluxLiveEditExtension {
                             }
                         }
                     }
-
-
                 });
+            }
+        });
+    }
 
+    private boolean connectIfFluxMicroservice(MachineProcessDto descriptor) {
+        if (descriptor == null) {
+            return false;
+        }
+        if ("flux".equals(descriptor.getName())) {
+            String urlToSubstitute = "http://${server.port.3000/tcp}";
+            if (commandPropertyValueProviderRegistry == null) {
+                return false;
+            }
+            substituteAndConnect(urlToSubstitute);
+            return true;
+        }
+        return false;
+    }
+
+    int trySubstitude = 10;
+
+    public void substituteAndConnect(final String commandLine) {
+        try {
+            String cmdLine = commandLine;
+            List<CommandPropertyValueProvider> providers = commandPropertyValueProviderRegistry.getProviders();
+            for (CommandPropertyValueProvider provider : providers) {
+                cmdLine = cmdLine.replace(provider.getKey(), provider.getValue());
+            }
+            connectToFlux(cmdLine);
+            return;
+        } catch (Exception e) {
+            if (trySubstitude > 0) {
+                new Timer() {
+                    @Override
+                    public void run() {
+                        substituteAndConnect(commandLine);
+                    }
+                }.schedule(1000);
+                return;
+            }
+            throw e;
+        }
+    }
+
+    int retryConnectToFlux = 5;
+
+    protected void connectToFlux(final String url) {
+
+        final SocketIOOverlay io = getSocketIO();
+        Log.info(getClass(), "connecting to " + url);
+
+        socket = io.connect(url);
+        socket.on("error", new Runnable() {
+            @Override
+            public void run() {
+                Log.info(getClass(), "error connecting to " + url);
             }
         });
 
+        socket.on("liveResourceChanged", new Consumer<FluxResourceChangedEventDataOverlay>() {
+            @Override
+            public void accept(FluxResourceChangedEventDataOverlay event) {
+                Document document = liveDocuments.get("/" + event.getProject() + "/" + event.getResource());
+                if (document == null) {
+                    return;
+                }
+                String addedCharacters = event.getAddedCharacters();
+                isUpdatingModel = true;
+                TextPosition cursorPosition = document.getCursorPosition();
+                document.replace(event.getOffset(), event.getRemovedCharCount(), addedCharacters);
+                document.setCursorPosition(cursorPosition);
+                isUpdatingModel = false;
+            }
+        });
 
+        socket.emit("connectToChannel", JsonUtils.safeEval("{\"channel\" : \"USER\"}"));
+    }
+
+    public static native SocketIOOverlay getSocketIO()/*-{
+                                                      return $wnd.io;
+                                                      }-*/;
+
+    private void connectToFluxOnFluxProcessStarted() {
         String machineId = appContext.getDevMachineId();
         final Unmarshallable<MachineProcessEvent> unmarshaller = dtoUnmarshallerFactory.newWSUnmarshaller(MachineProcessEvent.class);
         final String processStateChannel = "machine:process:" + machineId;
         final MessageHandler handler = new SubscriptionHandler<MachineProcessEvent>(unmarshaller) {
             @Override
             protected void onMessageReceived(MachineProcessEvent result) {
-                Log.info(getClass(), "process event" + result);
-
                 if (MachineProcessEvent.EventType.STARTED.equals(result.getEventType())) {
                     final int processId = result.getProcessId();
                     machineServiceClient.getProcesses(appContext.getDevMachineId()).then(new Operation<List<MachineProcessDto>>() {
@@ -132,8 +229,7 @@ public class CheFluxLiveEditExtension {
 
                             for (final MachineProcessDto machineProcessDto : descriptors) {
                                 if (machineProcessDto.getPid() == processId) {
-                                    Log.info(getClass(), "Started Process" + machineProcessDto);
-                                    new Timer() {
+                                     new Timer() {
                                         @Override
                                         public void run() {
                                             if (connectIfFluxMicroservice(machineProcessDto)) {
@@ -156,8 +252,17 @@ public class CheFluxLiveEditExtension {
             }
         };
         wsSubscribe(processStateChannel, handler);
+    }
 
+    private void wsSubscribe(String wsChannel, MessageHandler handler) {
+        try {
+            messageBus.subscribe(wsChannel, handler);
+        } catch (WebSocketException e) {
+            Log.error(getClass(), e);
+        }
+    }
 
+    private void sendFluxMessageOnDocumentModelChanged() {
         eventBus.addHandler(DocumentReadyEvent.TYPE, new DocumentReadyHandler() {
             @Override
             public void onDocumentReady(DocumentReadyEvent event) {
@@ -191,108 +296,6 @@ public class CheFluxLiveEditExtension {
                 });
             }
         });
-
-    }
-
-    private void injectSocketIO() {
-        SocketIOResources ioresources = GWT.create(SocketIOResources.class);
-        ScriptInjector.fromString(ioresources.socketIo().getText()).setWindow(ScriptInjector.TOP_WINDOW).inject();
-    }
-
-    private boolean connectIfFluxMicroservice(MachineProcessDto descriptor) {
-        if (descriptor == null) {
-            return false;
-        }
-        if ("flux".equals(descriptor.getName())) {
-            String urlToSubstitute = "http://${server.port.3000/tcp}";
-            if (commandPropertyValueProviderRegistry == null) {
-                return false;
-            }
-            substituteAndConnect(urlToSubstitute);
-            return true;
-        }
-
-        return false;
-
-    }
-
-    int retryConnectToFlux = 5;
-
-    protected void connectToFlux(final String url) {
-
-        final SocketIOOverlay io = getSocketIO();
-        Log.info(getClass(), "connecting to " + url);
-
-        socket = io.connect(url);
-        socket.on("error", new Runnable() {
-
-            @Override
-            public void run() {
-                Log.info(getClass(), "error connecting to " + url);
-            }
-        });
-
-
-        socket.on("liveResourceChanged", new Consumer<FluxResourceChangedEventDataOverlay>() {
-            @Override
-            public void accept(FluxResourceChangedEventDataOverlay event) {
-                Document document = liveDocuments.get("/" + event.getProject() + "/" + event.getResource());
-                if (document == null) {
-                    return;
-                }
-                String addedCharacters = event.getAddedCharacters();
-                isUpdatingModel = true;
-                TextPosition cursorPosition = document.getCursorPosition();
-                document.replace(event.getOffset(), event.getRemovedCharCount(), addedCharacters);
-                document.setCursorPosition(cursorPosition);
-                isUpdatingModel = false;
-            }
-        });
-
-        socket.emit("connectToChannel", JsonUtils.safeEval("{\"channel\" : \"USER\"}"));
-
-
-    }
-
-
-    public static native SocketIOOverlay getSocketIO()/*-{
-                                                      return $wnd.io;
-                                                      }-*/;
-
-    private void wsSubscribe(String wsChannel, MessageHandler handler) {
-        try {
-            messageBus.subscribe(wsChannel, handler);
-        } catch (WebSocketException e) {
-            Log.error(getClass(), e);
-        }
-    }
-
-    int trySubstitude = 10;
-
-    public void substituteAndConnect(final String commandLine) {
-        try {
-            String cmdLine = commandLine;
-            List<CommandPropertyValueProvider> providers = commandPropertyValueProviderRegistry.getProviders();
-
-            for (CommandPropertyValueProvider provider : providers) {
-                cmdLine = cmdLine.replace(provider.getKey(), provider.getValue());
-            }
-
-            connectToFlux(cmdLine);
-            return;
-        } catch (Exception e) {
-            Log.info(getClass(), e, "retrying " + trySubstitude--);
-            if (trySubstitude > 0) {
-                new Timer() {
-                    @Override
-                    public void run() {
-                        substituteAndConnect(commandLine);
-                    }
-                }.schedule(1000);
-                return;
-            }
-            throw e;
-        }
     }
 
 }
