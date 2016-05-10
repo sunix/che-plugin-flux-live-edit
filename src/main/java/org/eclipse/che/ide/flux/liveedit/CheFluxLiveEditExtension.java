2b1d0e1958af7add9b14e7eb54cb3e8637ca2cd5
Sun Seng David TAN <sunix@sunix.org>
2016-05-10 16:03:43 +0000

    update copyright year

--- src/main/java/org/eclipse/che/ide/flux/liveedit/CheFluxLiveEditExtension.java
+++ src/main/java/org/eclipse/che/ide/flux/liveedit/CheFluxLiveEditExtension.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Serli SAS.
+ * Copyright (c) 2016 Serli SAS.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
